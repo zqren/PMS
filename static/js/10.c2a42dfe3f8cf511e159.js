2d612d7431faa3f65d12bd70f7248a2ee47ff4e8
zqren <lxrcshik@sina.com>
2018-05-13 23:51:56 +0000

    TravisCI

---
+++ static/js/10.c2a42dfe3f8cf511e159.js
@@ -0,0 +1,2 @@
+webpackJsonp([10],{"6N4/":function(e,t,a){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var n={name:"weightManage",components:{pageTitle:a("jEgR").a}},i={render:function(){var e=this.$createElement,t=this._self._c||e;return t("div",{staticClass:"weight-management"},[t("page-title",{attrs:{title:"减重管理"}}),this._v(" "),t("router-view")],1)},staticRenderFns:[]};var r=a("VU/8")(n,i,!1,function(e){a("PA3T")},"data-v-74beaae1",null);t.default=r.exports},PA3T:function(e,t){}});
+//# sourceMappingURL=10.c2a42dfe3f8cf511e159.js.map
